bf1b3a6829b6d18dca17d21e28e513251b80a71a
XuanKhang <haxuankhang194@gmail.com>
2024-09-30 20:38:12 +0700

    First commit

---
+++ src/test/java/com/example/POD_BookingSystem/PodBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.POD_BookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
